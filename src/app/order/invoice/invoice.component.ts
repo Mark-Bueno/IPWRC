95ad864397a95616ee17de7e69c8372e0e717973
Mark Bueno <markbuenoguzman@hotmail.com>
2022-12-28 03:40:08 +0100

    changed invoice

--- src/app/order/invoice/invoice.component.ts
+++ src/app/order/invoice/invoice.component.ts
@@ -18,24 +18,11 @@ export class InvoiceComponent implements OnInit {
 
   ngOnInit() {
     this.orderProducts = this.data.orderProducts;
-    this.totalPrice = this.calculateTotalPrice();
     this.order = this.data.order;
   }
 
   onNoClick(): void {
     this.dialogRef.close();
-  }
-
-  calculateTotalPrice() {
-    const orderProducts = this.orderProducts;
-    let totalPrice = 0;
-    let i;
-    for (i = 0; i < orderProducts.length; i++) {
-      totalPrice = totalPrice + orderProducts[i].productPrice * orderProducts[i].amount;
-    }
-    return totalPrice;
-
-
   }
 
 }
