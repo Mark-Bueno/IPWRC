ce15b48ad3cc85c2c26f501bf15210a9b196ff49
Mark Bueno <markbuenoguzman@hotmail.com>
2023-01-02 03:30:16 +0100

    fixed env

--- src/app/shared/enviroment/enviroment.ts
+++ src/app/shared/enviroment/enviroment.ts
@@ -1,5 +1,5 @@
 export const enviroment = {
   production: true,
   // baseUrl: 'http://localhost:8080'
-  baseUrl: 'https://ipwrc-backend-production.up.railway.app'
+  baseUrl: 'iprwc-backend-production.up.railway.app'
 };
